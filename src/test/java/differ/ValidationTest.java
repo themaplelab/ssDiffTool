a949406f611f211827cda238aae17a44a8dc6027
Kristen Newbury <knewbury@ualberta.ca>
2020-11-02 19:22:52 -0500

    add test for monomorphic refs for an added method

--- src/test/java/differ/ValidationTest.java
+++ src/test/java/differ/ValidationTest.java
@@ -18,12 +18,11 @@ public class ValidationTest {
 	    Class<?> clazz = TestSetup.validateClassFile(mainClass);
 	    java.lang.reflect.Method methodHandle = clazz.getMethod("calcSquare", int.class);
 	    Assert.assertEquals(16, methodHandle.invoke(null, 4));  
-		
+	    TestSetup.testSetupRefresh();
 	}
 
     @Test
     public void testRunFieldAddition() throws Throwable {
-	G.reset();
 	String mainClass = "testexamples.AdditionFieldTest";
 	String[] classes = {mainClass};
 
@@ -42,7 +41,24 @@ public class ValidationTest {
 	java.lang.reflect.Method methodHandleTwo = clazz.getMethod("returnAValue");
 	Assert.assertNotNull(methodHandleTwo);
         Assert.assertEquals(1, methodHandleTwo.invoke(obj));
+	TestSetup.testSetupRefresh();
     }
 
+    @Test
+     public void testRunMethodAdditionMonomorphic() throws Throwable {
+	String mainClass = "testexamples.AdditionMethodMonomorphicTest";
+	String[] classes = {mainClass};
 
+        TestSetup.makeListFiles(classes);
+        String[] differArgs =  TestSetup.setup(mainClass);
+        TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
+
+        Class<?> clazz = TestSetup.validateClassFile(mainClass);
+
+        Object obj = clazz.newInstance();
+
+        java.lang.reflect.Method methodHandleOne = clazz.getMethod("returnSamePlusSeven", int.class);
+        Assert.assertNotNull(methodHandleOne);
+        Assert.assertEquals(8, methodHandleOne.invoke(obj, 1));
+    }
 }
