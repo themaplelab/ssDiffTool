0f3861fd9dfafc775689061d70118be432b0c176
Kristen Newbury <knewbury@ualberta.ca>
2020-11-18 00:52:10 -0500

    refactor test setup
    add one test specifically for non top level caller
    method as main class but with fullDir
    and refactor rest of tests for that change
    also add test refresh delay for in between setups
    to assure soot has time to reset
    also fix alphabetical setting on
    test run order to actually work now

--- src/test/java/differ/TestSetup.java
+++ src/test/java/differ/TestSetup.java
@@ -7,8 +7,10 @@ import java.io.File;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.ArrayList;
 import java.lang.Thread;
+
+import java.util.concurrent.TimeUnit;
 
 import soot.G;
 import differ.SemanticDiffer;
@@ -20,7 +22,9 @@ public class TestSetup {
     private static final String output = resources +"/adapterOutput"; //for final output            
     private static final String renamedDir = resources + "/renamedOriginals"; //for first setup patch adapter output                                                                                       
     private static final String normalCp = prefix + "/target/classes/";
-    private static final String redefDir = prefix +"/src/main/java/patch"; //for patch classes 
+    private static final String redefDirBase = prefix +"/src/main/java/testexamplespatch/"; //for patch classes 
+    private static final String redefDirSuffix = "/patch";
+    private static String redefDir;
     
     public static void makeListFiles(String[] classes) throws Throwable {
 	String mainClass = classes[0];
@@ -32,8 +36,11 @@ public class TestSetup {
 	writer.close();
     }
     
-    public static String[] setup(String mainClass) throws Throwable {
+    public static String[] setup(String mainClass, String redefDirSpecificPath, boolean useMainClass) throws Throwable {
 	//setup stuff
+
+	redefDir = redefDirBase + redefDirSpecificPath + redefDirSuffix;
+	
 	File adapterOut = new File(output);
 	adapterOut.mkdirs();
 	File renameOut = new File(renamedDir);
@@ -43,8 +50,13 @@ public class TestSetup {
 	String jce = System.getProperty("java.home") + "/lib/jce.jar";
 	String rt = System.getProperty("java.home") + "/lib/rt.jar";
 	String cp = normalCp + ":" + redefDir + ":" + rt + ":" + jce;
+
+	String[] differArgs = { "-cp", cp, "-w", "-renameDestination", renamedDir, "-finalDestination", output, "-redefcp", redefDir, "-runRename", "true", "-useFullDir", "false", "-mainClass", mainClass, "-originalclasslist", prefix + "/" + mainClass + ".originalclasses.out", "Example"
+};
+	if(!useMainClass){
+	    differArgs[12] = "true";
+	}
 	
-	String[] differArgs = { "-cp", cp, "-w", "-renameDestination", renamedDir, "-finalDestination", output, "-redefcp", redefDir, "-runRename", "true", "-mainClass", mainClass, "-originalclasslist", prefix + "/" + mainClass + ".originalclasses.out", "Example"};
 	System.out.println("Command Line: " + Arrays.toString(differArgs));
 	return differArgs;
 	
@@ -57,6 +69,11 @@ public class TestSetup {
 	deleteFiles(renamedDir+ "/" +packagePrefix);
 	//reset Soot, aka the Scene and loaded classes
 	G.reset();
+	try {
+	    TimeUnit.SECONDS.sleep(5); //possibly not reset if next run starts at exact same second at prev
+	} catch(Exception e) {
+	    System.out.println("Could not delay refresh: " + e.getMessage());
+	}
     }
 
     private static void deleteFiles(String path){
--- src/test/java/differ/ValidationTest.java
+++ src/test/java/differ/ValidationTest.java
@@ -2,31 +2,34 @@ import java.io.File;
 import java.util.Arrays;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ValidationTest {
 
 	@Test
-	public void aTestRunRename() throws Throwable {
-	    String mainClass = "testexamples.NoChangeTest";
+	public void ATestRunRename() throws Throwable {
+	    String mainClass = "testexamples.nochange.NoChangeTest";
 	    String[] classes = {mainClass};	    
 
 	    TestSetup.makeListFiles(classes);	    
-	    String[] differArgs =  TestSetup.setup(mainClass);
+	    String[] differArgs =  TestSetup.setup(mainClass, "nochange", true);
 	    TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
 	    
 	    Class<?> clazz = TestSetup.validateClassFile(mainClass);
 	    java.lang.reflect.Method methodHandle = clazz.getMethod("calcSquare", int.class);
 	    Assert.assertEquals(16, methodHandle.invoke(null, 4));  
-	    TestSetup.testSetupRefresh("testexamples/");
+	    TestSetup.testSetupRefresh("testexamples/nochange");
 	}
 
     @Test
-    public void bTestRunFieldAddition() throws Throwable {
-	String mainClass = "testexamples.AdditionFieldTest";
+    public void BTestRunFieldAddition() throws Throwable {
+	String mainClass = "testexamples.fieldaddition.AdditionFieldTest";
 	String[] classes = {mainClass};
 
 	TestSetup.makeListFiles(classes);
-	String[] differArgs =  TestSetup.setup(mainClass);
+	String[] differArgs =  TestSetup.setup(mainClass, "fieldaddition", true);
 	TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
 	
 	Class<?> clazz = TestSetup.validateClassFile(mainClass);
@@ -40,16 +43,16 @@ public class ValidationTest {
 	java.lang.reflect.Method methodHandleTwo = clazz.getMethod("returnAValue");
 	Assert.assertNotNull(methodHandleTwo);
         Assert.assertEquals(1, methodHandleTwo.invoke(obj));
-	TestSetup.testSetupRefresh("testexamples/");
+	TestSetup.testSetupRefresh("testexamples/fieldaddition");
     }
 
     @Test
-     public void cTestRunMethodAdditionMonomorphic() throws Throwable {
+     public void CTestRunMethodAdditionMonomorphic() throws Throwable {
 	String mainClass = "testexamples.methodadditionmonomorphic.AdditionMethodMonomorphicTest";
 	String[] classes = {mainClass};
 
         TestSetup.makeListFiles(classes);
-        String[] differArgs =  TestSetup.setup(mainClass);
+        String[] differArgs =  TestSetup.setup(mainClass, "methodadditionmonomorphic", true);
         TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
 
         Class<?> clazz = TestSetup.validateClassFile(mainClass);
@@ -63,18 +66,18 @@ public class ValidationTest {
     }
     
     @Test
-    public void dTestRunMethodAdditionPolymorphic() throws Throwable {
+    public void DTestRunMethodAdditionPolymorphic() throws Throwable {
 	String mainClass = "testexamples.methodadditionpolymorphic.UserPolyTest";
-        String[] classes = {mainClass,
+	String[] classes = {mainClass,
 			    "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOne",
 			    "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThree",
 			    "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSix"};
 
         TestSetup.makeListFiles(classes);
-        String[] differArgs =  TestSetup.setup(mainClass);
+        String[] differArgs =  TestSetup.setup(mainClass, "methodadditionpolymorphic", true);
         TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
 
-        Class<?> clazz = TestSetup.validateClassFile(mainClass);
+        Class<?> clazz = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.UserPolyTest");
 
         Object obj = clazz.newInstance();
 	
@@ -87,11 +90,69 @@ public class ValidationTest {
         Assert.assertEquals(6, methodHandleOne.invoke(obj, 6));
 	Assert.assertEquals(2, methodHandleOne.invoke(obj, 2));
         Assert.assertEquals(2, methodHandleOne.invoke(obj, 4));
+
+	Class<?> childOne = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOneNewClass");
+        java.lang.reflect.Method methodHandleChildOne = childOne.getMethod("emitMethod");
+        Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOneNewClass", methodHandleChildOne.getDeclaringClass().getName());
+
+	Class<?> childThree = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThreeNewClass");
+        java.lang.reflect.Method methodHandleChildThree = childThree.getMethod("emitMethod");
+        Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThreeNewClass", methodHandleChildThree.getDeclaringClass().getName());
+
+	Class<?> childSix = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSixNewClass");
+        java.lang.reflect.Method methodHandleChildSix = childSix.getMethod("emitMethod");
+        Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSixNewClass", methodHandleChildSix.getDeclaringClass().getName());
+
+	
 	TestSetup.testSetupRefresh("testexamples/methodadditionpolymorphic");
     }
 
     @Test
-    public void fTestRunMethodRemoval() throws Throwable {
+    public void ETestRunMethodAdditionPolymorphic() throws Throwable {
+	//tests the process dir approach of cg contruction
+	String mainClass = "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOne";
+        String[] classes = {mainClass,
+			    "testexamples.methodadditionpolymorphic.UserPolyTest",
+                            "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThree",
+                            "testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSix"};
+
+        TestSetup.makeListFiles(classes);
+        String[] differArgs =  TestSetup.setup(mainClass, "methodadditionpolymorphic", false);
+        TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
+
+        Class<?> clazz = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.UserPolyTest");
+
+        Object obj = clazz.newInstance();
+
+        java.lang.reflect.Method methodHandleOne = clazz.getMethod("use", int.class);
+        Assert.assertNotNull(methodHandleOne);
+        Assert.assertEquals(0, methodHandleOne.invoke(obj, 0));
+        Assert.assertEquals(1, methodHandleOne.invoke(obj, 1));
+        Assert.assertEquals(1, methodHandleOne.invoke(obj, 5));
+	Assert.assertEquals(3, methodHandleOne.invoke(obj, 3));
+        Assert.assertEquals(6, methodHandleOne.invoke(obj, 6));
+        Assert.assertEquals(2, methodHandleOne.invoke(obj, 2));
+        Assert.assertEquals(2, methodHandleOne.invoke(obj, 4));
+
+        Class<?> childOne = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOneNewClass");
+        java.lang.reflect.Method methodHandleChildOne = childOne.getMethod("emitMethod");
+        Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildOneNewClass", methodHandleChildOne.getDeclaringClass().getName());
+
+        Class<?> childThree = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThreeNewClass");
+        java.lang.reflect.Method methodHandleChildThree = childThree.getMethod("emitMethod");
+        Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildThreeNewClass", methodHandleChildThree.getDeclaringClass().getName());
+	
+    Class<?> childSix = TestSetup.validateClassFile("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSixNewClass");
+    java.lang.reflect.Method methodHandleChildSix = childSix.getMethod("emitMethod");
+    Assert.assertEquals("testexamples.methodadditionpolymorphic.AdditionMethodPolymorphicTestChildSixNewClass", methodHandleChildSix.getDeclaringClass().getName());
+
+
+    TestSetup.testSetupRefresh("testexamples/methodadditionpolymorphic");
+
+    }
+
+    @Test
+    public void FTestRunMethodRemoval() throws Throwable {
         String mainClass = "testexamples.methodremoval.UserPolyTest";
         String[] classes = {mainClass,
                             "testexamples.methodremoval.AdditionMethodPolymorphicTestChildOne",
@@ -99,7 +160,7 @@ public class ValidationTest {
                             "testexamples.methodremoval.AdditionMethodPolymorphicTestChildSix"};
 
         TestSetup.makeListFiles(classes);
-        String[] differArgs =  TestSetup.setup(mainClass);
+        String[] differArgs =  TestSetup.setup(mainClass, "methodremoval", true);
         TestSetup.runAdapter(Arrays.asList(mainClass), differArgs);
 
         Class<?> clazz = TestSetup.validateClassFile(mainClass);
@@ -128,7 +189,7 @@ public class ValidationTest {
         java.lang.reflect.Method methodHandleRMSix = childSix.getMethod("emitMethod");
         Assert.assertEquals("testexamples.methodremoval.AdditionMethodPolymorphicTestChildSix", methodHandleRMSix.getDeclaringClass().getName());
 	
-	//TestSetup.testSetupRefresh("testexamples/methodremoval");
+	TestSetup.testSetupRefresh("testexamples/methodremoval");
     }
 }
 
